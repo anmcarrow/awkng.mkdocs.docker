94bc0cca9ec40319026838b01ca5d3d3015c86e5
Andrey Makarov <mb@mcrrw.me>
2020-08-22 17:40:46 +0300

    Update health check script

--- src/scripts/health.sh
+++ src/scripts/health.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PING_URI="http://localhost:${HEALTHCHECK_PORT:-8000}/"
+PING_URL="http://localhost:${HEALTHCHECK_PORT:-8000}/"
 PING_RESPONSE="$(wget -O- --timeout=10 -q $PING_URL)"
 
 if (echo -e "$PING_RESPONSE" | grep 'MkDocs version' > /dev/null 2>&1); then
